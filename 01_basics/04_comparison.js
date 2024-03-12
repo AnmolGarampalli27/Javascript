2b566e28495c17bf248e497f4f591777e8492dcf
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-12 21:45:31 +0530

    Comparison Operator in Javascript

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log("2" > 1);  //automatic datatype conversion
+console.log("2" < 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null < 0);
+
+console.log("2" == 2); //true data comparison
+console.log("2" === 2); // data as well as data type comparison
