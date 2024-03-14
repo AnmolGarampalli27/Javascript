635781ba653666d171386b95deca30392b4b948b
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-15 01:12:47 +0530

    Numbers & Maths in Javascript

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,12 @@
+/** @format */
+const age = 25;
+
+const newNumber = new Number(100);
+
+console.log(typeof age);
+console.log(typeof newNumber);
+
+console.log(newNumber.toString());
+console.log(
+  newNumber.toFixed(4)
+); /* This method is very commonly used in E-Commerce application to make calculations accurate to n digits */
