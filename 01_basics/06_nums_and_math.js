ebc9132bac12a876d2f0272ff403c8fadf237811
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-15 21:49:07 +0530

    Completing Number & Maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -22,3 +22,32 @@ console.log(someNumber.toPrecision(2));
 const largeNumber = 100000000;
 
 console.log(largeNumber.toLocaleString());
+
+// ++++++++++++++++  Maths +++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); //converts negative only values to positve values
+
+console.log(Math.round(6.6765));
+
+console.log(Math.ceil(6.33)); //chooses top value after round off
+
+console.log(Math.floor(6.53)); //chooses tthe low value after round off
+
+console.log(Math.min(12, 23, 40, 10, 13));
+
+console.log(Math.max(10, 24, 90, 345, 500));
+
+console.log(Math.random());
+
+console.log(Math.random() * 10 + 1);
+
+const maxLimit = 20;
+const minLimit = 10;
+
+const genNum = Math.floor(Math.random() * (maxLimit - minLimit + 1) + minLimit);
+
+console.log(genNum);
+
+// Math.random method gives you value between 1 & 10
+
