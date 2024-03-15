f53dc5f362153900f9410716a986386ec1f5d166
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-15 11:47:59 +0530

    Numbers and Maths in Javascript

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -10,3 +10,15 @@ console.log(newNumber.toString());
 console.log(
   newNumber.toFixed(4)
 ); /* This method is very commonly used in E-Commerce application to make calculations accurate to n digits */
+
+// Precision Method
+
+const someNumber = 100.4745;
+
+console.log(someNumber.toPrecision(2));
+
+// Understanding Locale String
+
+const largeNumber = 100000000;
+
+console.log(largeNumber.toLocaleString());
