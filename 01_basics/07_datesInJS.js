fd17fe403601fbe3bb96ba70f9e1836d43556599
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-17 11:02:54 +0530

    Last minute changes

--- 01_basics/07_datesInJS.js
+++ 01_basics/07_datesInJS.js
@@ -1,25 +1,30 @@
 /** @format */
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
 let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
-console.log(myDate); //unreadble format
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023");
+// console.log(myCreatedDate.toLocaleString());
 
-console.log(myDate.toString()); //slightly readable format
+let myTimeStamp = Date.now();
 
-console.log(myDate.toDateString());
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
 
-console.log(myDate.toLocaleDateString());
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
 
-console.log(myDate.toISOString());
+// `${newDate.getDay()} and the time `
 
-console.log(myDate.toJSON());
-
-console.log(myDate.toLocaleString());
-
-console.log(typeof myDate); // Type is Object because we created a new object
-
-let myCreatedDate = new Date(2024, 11, 23);
-console.log(myCreatedDate.toDateString());
-
-console.log(myCreatedDate.toLocaleString());
+newDate.toLocaleString("default", {
+  weekday: "long",
+});
