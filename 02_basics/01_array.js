225dc3bc69f136ec38f08a69a0301c7da33e1b95
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-17 11:24:40 +0530

    Initial Commit

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -3,3 +3,21 @@
 //  (): paranthesis
 //  []: brackets
 // {} : braces
+
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array :Array documentation
+
+const myArr = [0, 1, 2, 3, 4, 5, 13, 14, 25, 233, 40];
+
+console.log(typeof myArr);
+
+const myHeroes = ["Shaktiman", "Naagraj"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5);
+
+console.log(typeof myArr2);
+console.log(myArr2);
+
+console.log(myArr[8]); // Accessing elements in the array
+
+
+// Array Methods
