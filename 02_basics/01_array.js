4f992eda175c7abfcb6e5a7dea602e7f610bd40f
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-17 11:55:16 +0530

    Array Methods

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -19,5 +19,39 @@ console.log(myArr2);
 
 console.log(myArr[8]); // Accessing elements in the array
 
-
 // Array Methods
+
+// Array Push Method
+
+myArr.push(6);
+console.log(myArr);
+
+// Adding  another element
+
+myArr.push(7);
+console.log(myArr);
+
+myArr.pop();
+console.log(myArr);
+
+myArr.unshift(10);
+console.log(myArr);
+
+myArr.shift();
+console.log(myArr);
+
+console.log(myArr.includes(9));
+
+console.log(myArr.includes(0));
+
+console.log(myArr.indexOf(4));
+
+const newArray = myArr.join();
+console.log(typeof newArray);
+
+// Slice, Splice method
+
+console.log("A", myArr);
+const myNewArray = myArr.slice(1, 3);
+
+console.log(myNewArray);
