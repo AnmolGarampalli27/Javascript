1ebb2249b0785a4e19c6df7c854f6cde7635d607
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-17 13:48:18 +0530

    Array methods

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -12,3 +12,17 @@ var herosOfUniverse = marvelHeroes.concat(dcHeroes);
 // console.log(marvelHeroes);
 
 console.log("Combined heros of the Universe are " + herosOfUniverse);
+
+
+const allNewHeros = [...dcHeroes, ...marvelHeroes];
+
+console.log(allNewHeros);
+
+const another_array =  [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+const real_another_array = another_array.flat(Infinity);
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat :Array flat method
+
+console.log(real_another_array);
+
+console.log(Array.from("Anmol Garampalli"))
