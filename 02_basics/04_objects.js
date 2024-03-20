a4d5efad7f224bffac0b73cb6bf5a894891605fd
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-20 20:00:51 +0530

    minor changes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,10 +1,25 @@
 /** @format */
 
 const tinderUser = new Object();
+tinderUser.id = "123abc";
+tinderUser.name = "Michael";
+tinderUser.isLoggedIn = false;
 
-console.log(tinderUser);
+// console.log(tinderUser);
 // O/P: {} Empty Object
 
 const tinderUser2 = {};
 console.log(tinderUser2);
 // O/P: Empty Object
+
+const regularUser = {
+  email: "name@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "Anmol",
+      lastname: "Garampalli",
+    },
+  },
+};
+
+console.log(regularUser.fullname.userfullname.firstname);
