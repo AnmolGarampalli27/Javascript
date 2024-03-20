0788ad14959026ecbc61bfcfc62b035175740352
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-20 21:37:27 +0530

    combining outputs

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -44,3 +44,8 @@ const obj4 = Object.assign({}, obj1, obj2); // {} is an optional parameter but i
 console.log(obj4); // O/P: { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 // The Object.assign() static method copies all
 //  enumerable own properties from one or more source objects to a target object. It returns the modified target object.
+
+const obj5 = Object.assign({}, obj1, obj2, obj3);
+console.log(obj5);
+
+//  In case paranthesis is not used all the elements will be stored in intial array. In this case it will obj1.
