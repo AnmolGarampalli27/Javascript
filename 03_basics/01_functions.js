42e8d97e087b78d27fce28c45e6ef1d75f81cf01
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-21 10:17:04 +0530

    Funnctions in Javascript

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,6 +23,19 @@ L
 function addTwoNumbers(number_a, number_b) {
   //number_a,b are parameters
   console.log(number_a + number_b);
+
+  return number_a + number_b;
 }
 
 addTwoNumbers(1, 2); //O/P:3 1,2 are arguments to the function
+
+function loginUserMessage(username = "sam") {
+  //default value is sam
+  if (!username) {
+    console.log("Please enter a username");
+    return;
+  }
+  return `${username} just logged in`;
+}
+
+console.log(loginUserMessage("Anmol Garampalli"));
