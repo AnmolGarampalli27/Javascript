bb683c53f1398d06a8299b1cab1ab0e2759f96e5
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-24 12:56:23 +0530

    Functions in depth

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,3 +39,17 @@ function loginUserMessage(username = "sam") {
 }
 
 console.log(loginUserMessage("Anmol Garampalli"));
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000)); //O/P: [ 500, 2000 ]
+
+const user = {
+  username: "Anmol",
+  price: 199,
+};
+
+function handleobject(anyobject) {}
+console.log("Username is ${anyobject.username} & price is ${anyobject.price}");
