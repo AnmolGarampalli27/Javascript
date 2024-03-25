601c77049e574b38d2d61ab2e2c764fdc5aef426
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-03-25 22:17:40 +0530

    Program flow in Javascript

---
+++ Program Flow/javascriptExecution.js
@@ -0,0 +1,16 @@
+/** @format */
+
+function one() {
+  console.log("one");
+  two();
+}
+function two() {
+  console.log("two");
+  three();
+}
+function three() {
+  console.log("three");
+}
+one();
+two();
+three();
